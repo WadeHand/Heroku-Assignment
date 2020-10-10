5043a69755ab1de609b2fa829a6242607f652fec
WadeHand <whand@gus.pittstate.edu>
2020-10-10 13:18:01 -0500

    Added contollers
    Added forms and controllers.

--- controllers/createController.js
+++ controllers/createController.js
@@ -1,15 +1,43 @@
+
+exports.get_create_render = function(req, res) {
+    res.render('birds/birds');
+}
 exports.get_create_create = function(req, res) {
-    res.render('create/create');
+res.render('birds/create');
+}
+exports.post_create_create = function(req, res) {
+    let newCreate = {       
+        Species: req.body.Species,       
+        Nickname: req.body.Nickname,
+        Status: req.body.Status,
+        };    
+       
+        console.log(newCreate);
+        res.redirect('birds/congrats');
+    }
+exports.get_create_congrats = function(req, res) {
+    res.render('birds/congrats');
 }
 
-exports.create_post = function (req, res) {    
-let newCreate = {       
-    content: req.body.content,       
-    completd: false    
-    };    
- };
+//exports.get_create = function(req, res) {
+//    res.render('create/create');
+//    
+//}
+
+//exports.post_create_bird = function (req, res) {    
+ //   console.log("made it");
+  //  console.log(req.body.Species);
+  //  console.log(req.body.Nickname);
+  //  console.log(req.body.Status);
+
+//let newCreate = {       
+//    Species: req.body.Species,       
+ //   Nickname: req.body.Nickname,
+ //   Status: req.body.Status,
+  //  };    
+// };
 
 // we'd save to a database or send to an API here    
-    consol.log('this worked')
-    console.log(newCreate);    
-    res.render('create-created');
+   // console.log('this worked')
+    //console.log(newCreate);    
+    //res.render('create-created');
--- routes/birds.js
+++ routes/birds.js
@@ -1,29 +1,50 @@
 const express = require('express');
 const router = express.Router();
+const createController = require('../controllers/createController');
+
+router.get('/', createController.get_create_render);
+router.get('/create', createController.get_create_create);
+router.post('/create', createController.get_create_create);
+router.get('/congrats', createController.get_create_congrats);
+//get request
+//router.get('/create', function (req, res, next) {
+ //   res.render('birds/create');
+//});
+//post request
+//router.post('/create', function (req, res, next) {
+ //res.render('birds/congrats');
+
+
+ //   console.log("made it");
+//    console.log(req.body.Species);
+ //   console.log(req.body.Nickname);
+//    console.log(req.body.Status);
+//});
+
+//router.get('/create', createController.get_create_bird);
+
+//router.post('create', createController.post_create_bird);
+
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-    res.render('birds/birds');
-    let birdData = {
-        name: 'cool birdie',
-        type: 'bird',
-        species: 'hawk'
-    }
-  });
+//router.get('/', function (req, res, next) {
+  //  res.render('birds/birds');
+ //   let birdData = {
+//        name: 'cool birdie',
+ //       type: 'bird',
+  //      species: 'hawk'
+//    }
+//  });
   
-router.get('/', function (req, res, next) {
-  
-    let birdData = {
-        name: 'cool birdie',
-        type: 'bird',
-        species: 'hawk'
-    }
+//router.get('/', function (req, res, next) {
+ 
+ //   let birdData = {
+  //      name: 'cool birdie',
+  //      type: 'bird',
+  //      species: 'hawk'
+  //  }
 
-    res.send(birdData);
-});
-
-router.get('/create', function (req, res, next) {
-    res.render('birds/create');
-});
+  //  res.send(birdData);
+//});
 
 module.exports = router;
