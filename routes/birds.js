0a35f9880f45847d4185522d4fc30a5a37fb7bb0
WadeHand <whand@gus.pittstate.edu>
2020-10-09 20:39:29 -0500

    Added navbar and different pages
    Set up pages/added navbar. May want a congrats page for when you get the bird made but will look at that later. Need to add logic behind buttons and such.

--- routes/birds.js
+++ routes/birds.js
@@ -3,6 +3,15 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
+    res.render('birds/birds');
+    let birdData = {
+        name: 'cool birdie',
+        type: 'bird',
+        species: 'hawk'
+    }
+  });
+  
+router.get('/', function (req, res, next) {
   
     let birdData = {
         name: 'cool birdie',
