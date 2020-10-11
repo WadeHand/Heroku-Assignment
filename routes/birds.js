1809436c05a8f47097aa0b7200ac85eb4918029d
WadeHand <whand@gus.pittstate.edu>
2020-10-11 11:45:04 -0500

    Changed a .post to a .get
    That was the problem, it works now.

--- routes/birds.js
+++ routes/birds.js
@@ -4,47 +4,7 @@ const createController = require('../controllers/createController');
 
 router.get('/', createController.get_create_render);
 router.get('/create', createController.get_create_create);
-router.post('/create', createController.get_create_create);
+router.post('/create', createController.post_create_create);
 router.get('/congrats', createController.get_create_congrats);
-//get request
-//router.get('/create', function (req, res, next) {
- //   res.render('birds/create');
-//});
-//post request
-//router.post('/create', function (req, res, next) {
- //res.render('birds/congrats');
-
-
- //   console.log("made it");
-//    console.log(req.body.Species);
- //   console.log(req.body.Nickname);
-//    console.log(req.body.Status);
-//});
-
-//router.get('/create', createController.get_create_bird);
-
-//router.post('create', createController.post_create_bird);
-
-
-/* GET home page. */
-//router.get('/', function (req, res, next) {
-  //  res.render('birds/birds');
- //   let birdData = {
-//        name: 'cool birdie',
- //       type: 'bird',
-  //      species: 'hawk'
-//    }
-//  });
-  
-//router.get('/', function (req, res, next) {
- 
- //   let birdData = {
-  //      name: 'cool birdie',
-  //      type: 'bird',
-  //      species: 'hawk'
-  //  }
-
-  //  res.send(birdData);
-//});
 
 module.exports = router;
